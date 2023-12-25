bba1ce5d8955899720f504659ca764695d259fa9
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2023-12-25 14:34:34 +0900

    add Lorenz-Attractor sample by deno (and gnuplot)

---
+++ ts/Lorenz-Attractor.ts
@@ -0,0 +1,48 @@
+// deno run --allow-write Lorenz-Attractor.ts
+
+import { writeFile } from "https://deno.land/std@0.86.0/node/fs.ts";
+import { TypeParam, TypeFuncRes } from "./solve_ode.ts";
+import { SolveOde, dop853 } from "./solve_ode.ts";
+
+// https://en.wikipedia.org/wiki/Lorenz_system
+// ODE
+// dx/dt = sigma * (y - x)
+// dy/dt = x * (rho - z) - y
+// dz/dt = x * y - beta * z
+// param[sigma, rho, beta]
+
+function func(x: number[], param: TypeParam, _t: number): TypeFuncRes {
+    return [
+        param[0] * (x[1] - x[0]),
+        param[1] * x[0] - x[0] * x[2] - x[1],
+        x[0] * x[1] - param[2] * x[2]
+    ];
+}
+
+let param = [10, 28, 8/3];
+let init = [1, 0, 0];
+
+let [time, value] = SolveOde(
+    func,          // ODE, solver
+    dop853,        // solver
+    param,         // parameters
+    0, 100, 0.01,  // start, end, step
+    init           // initial values
+);
+
+
+// save results
+let csv_string  = ""; 
+for (let d of value) {
+    csv_string += d.join(",");
+    csv_string += "\n";
+}
+
+writeFile("result/Lorenz-Attractor.csv", csv_string, (err, value) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('write end');
+    }
+});
+
---
+++ ts/gnuplot/Lorenz-Attractor.plt
@@ -0,0 +1,4 @@
+set datafile separator ","
+splot "../result/Lorenz-Attractor.csv" with lines linecolor "blue"
+
+pause -1
